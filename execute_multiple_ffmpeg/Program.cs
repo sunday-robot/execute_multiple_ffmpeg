c8f60df0a9b9c932dd695ec3c0c40e9e8ef2f648
sunday_robot <sgx03150@nifty.com>
2023-06-15 00:39:12 +0900

    a

---
+++ execute_multiple_ffmpeg/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace execute_multiple_ffmpeg
+{
+    /// <summary>
+    /// コマンドライン引数に渡された各動画ファイルに対し、ffmepgを実行させる。
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
