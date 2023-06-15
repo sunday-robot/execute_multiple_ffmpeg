ba1934e66e3da1e2935d73a535ae5188da5f8322
sunday_robot <sgx03150@nifty.com>
2023-06-16 01:55:36 +0900

    a

--- execute_multiple_ffmpeg/Program.cs
+++ execute_multiple_ffmpeg/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,7 +15,30 @@ namespace execute_multiple_ffmpeg
     {
         static void Main(string[] args)
         {
+            foreach (var arg in args)
+            {
+                var r = Run(arg);
+                if (r != 0)
+                {
+                    Console.WriteLine($"{arg} : failed.({r})");
+                }
+            }
+            Console.ReadLine();
+        }
 
+        static int Run(string filePath)
+        {
+            var proc = new Process();
+
+            proc.StartInfo.FileName = @"C:\user\ffmpeg\bin\ffmpeg.exe";
+            proc.StartInfo.Arguments = "-i " + filePath
+                + " -c:v hevc_nvenc"
+                + " -b_ref_mode 0"
+                + " -cq 38"
+                + " " + filePath + ".h265_nv_cq_38.mp4";
+            proc.Start();
+            proc.WaitForExit();
+            return proc.ExitCode;
         }
     }
 }
