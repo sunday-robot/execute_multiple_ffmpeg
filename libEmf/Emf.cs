5aac7b8d7fe4307535cabac578eeecdacf410e0b
sunday robot <sgx03150@nifty.com>
2025-08-31 00:55:28 +0900

    a

--- libEmf/Emf.cs
+++ libEmf/Emf.cs
@@ -4,76 +4,47 @@ namespace libEmf
 {
     public static class Emf
     {
-        public static void Execute(string[] filePath, string encoderName, string[] options)
+        public static void Execute(IEnumerable<string> paths, string encoderName, string[] options)
         {
-            ForEachFile(
-                filePath,
-                // 動画ファイルかどうかの判定(拡張子のみで判定)
+            ForEachFileRecursively.Do(
+                paths,
                 filePath =>
                 {
-                    var ext = Path.GetExtension(filePath);
-                    switch (ext.ToUpper())
-                    {
-                        case ".AVI":
-                        case ".M4V":
-                        case ".MKV":
-                        case ".MP4":
-                        case ".MPEG":
-                        case ".MPG":
-                        case ".RMVB":
-                        case ".TS":
-                        case ".VOB":
-                        case ".WMV":
-                            return true;
-                        default:
-                            return false;
-                    }
-                },
-                // ffmpegの実行
-                filePath =>
-                {
-                    ExecuteFfmpeg(
-                        filePath,
-                        encoderName,
-                        options);
+                    if (!HasMovieFileExtension(filePath))
+                        return;
+
+                    ExecuteFfmpeg(filePath, encoderName, options);
                 });
             Console.WriteLine("Finished.");
             Console.WriteLine("Hit enter key.");
             Console.ReadLine();
         }
 
-        static void ForEachFile(string[] filePathList, Func<string, bool> isTarget, Action<string> processor)
+        /// <summary>
+        /// 動画ファイルかどうかを返す。拡張子で判断するだけのもの。
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        static bool HasMovieFileExtension(string filePath)
         {
-            foreach (var filePath in filePathList)
-                if (Directory.Exists(filePath))
-                    ProcessDirectory(filePath, isTarget, processor);
-                else
-                    ProcessFile(filePath, isTarget, processor);
-        }
-
-        static List<string> SortByName(IEnumerable<string> paths)
-        {
-            return paths.OrderBy(p => Path.GetFileName(p), StringComparer.OrdinalIgnoreCase).ToList();
-        }
-
-        static void ProcessDirectory(string parentDirectoryPath, Func<string, bool> isTarget, Action<string> processor)
-        {
-            foreach (var directoryPath in SortByName(Directory.EnumerateDirectories(parentDirectoryPath)))
-                ProcessDirectory(directoryPath, isTarget, processor);
-            foreach (var filePath in SortByName(Directory.EnumerateFiles(parentDirectoryPath)))
-                ProcessFile(filePath, isTarget, processor);
-        }
-
-        static void ProcessFile(string filePath, Func<string, bool> isTarget, Action<string> processor)
-        {
-            if (!File.Exists(filePath))
+            var ext = Path.GetExtension(filePath);
+            switch (ext.ToUpper())
             {
-                Console.WriteLine($"{filePath} does not exists.");
-                return;
+                case ".ASF":
+                case ".AVI":
+                case ".M4V":
+                case ".MKV":
+                case ".MP4":
+                case ".MPEG":
+                case ".MPG":
+                case ".RMVB":
+                case ".TS":
+                case ".VOB":
+                case ".WMV":
+                    return true;
+                default:
+                    return false;
             }
-            if (!isTarget(filePath))
-                return;
-            processor(filePath);
         }
 
         static void ExecuteFfmpeg(string inputFilePath, string encoderName, string[] options)
---
+++ libEmf/ForEachFileRecursively.cs
@@ -0,0 +1,30 @@
+﻿namespace libEmf
+{
+    internal static class ForEachFileRecursively
+    {
+        internal static void Do(IEnumerable<string> paths, Action<string> processor)
+        {
+            foreach (var e in paths)
+                if (Directory.Exists(e))
+                    ProcessDirectory(e, processor);
+                else
+                    processor(e);
+        }
+
+        static IEnumerable<string> SortByName(IEnumerable<string> paths)
+        {
+            return paths.OrderBy(e => Path.GetFileName(e), StringComparer.OrdinalIgnoreCase);
+        }
+
+        static void ProcessDirectory(string directoryPath, Action<string> processor)
+        {
+            var sortedSubDirectoryPaths = SortByName(Directory.EnumerateDirectories(directoryPath));
+            foreach (var e in sortedSubDirectoryPaths)
+                ProcessDirectory(e, processor);
+
+            var sortedFilePaths = SortByName(Directory.EnumerateFiles(directoryPath));
+            foreach (var e in sortedFilePaths)
+                processor(e);
+        }
+    }
+}
